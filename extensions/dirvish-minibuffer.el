d8ae8b56dad51a20bd3d30e6cd6c0e4612c4bb11
Alex <hellosimon1103@hotmail.com>
2021-12-05 22:18:04 +0800

    Rename project to dirvish

--- extensions/dirvish-minibuffer.el
+++ extensions/dirvish-minibuffer.el
@@ -1,11 +1,11 @@
-;;; danger-minibuffer.el --- minibuffer preview window -*- lexical-binding: t -*-
+;;; dirvish-minibuffer.el --- minibuffer preview window -*- lexical-binding: t -*-
 
 ;; Author: Alex Lu <alexluigit@gmail.com>
 ;; Maintainer: Alex Lu <alexluigit@gmail.com>
 ;; Created: 2021
 ;; Version: 0.1
-;; Package-Requires: ((emacs "27.1") (danger "0.7.0"))
-;; Homepage: https://github.com/alexluigit/danger
+;; Package-Requires: ((emacs "27.1") (dirvish "0.7.0"))
+;; Homepage: https://github.com/alexluigit/dirvish
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -24,88 +24,88 @@
 
 ;;; Commentary:
 
-;; This package is a Danger extension, which provides minibuffer file preview in a `danger' style.
+;; This package is a Dirvish extension, which provides minibuffer file preview in a `dirvish' style.
 
 ;;; Code:
 
 (declare-function selectrum--get-candidate "selectrum")
 (declare-function selectrum--get-full "selectrum")
 (declare-function vertico--candidate "vertico")
-(defvar danger-update--preview-timer)
+(defvar dirvish-update--preview-timer)
 
-(require 'danger)
+(require 'dirvish)
 
-(defcustom danger-minibuf-preview-position
+(defcustom dirvish-minibuf-preview-position
   (lambda (info)
     (cons (/ (- (plist-get info :parent-frame-width)
                 (plist-get info :posframe-width)) 2)
           (or (* (frame-parameter nil 'internal-border-width) 2) 60)))
   "doc"
-  :group 'danger :type 'function)
+  :group 'dirvish :type 'function)
 
-(defvar danger-minibuf-preview-categories '(file project-file)
+(defvar dirvish-minibuf-preview-categories '(file project-file)
   "doc")
 
-(defvar danger-minibuf-preview--height (- 1 (* max-mini-window-height 1.5))
+(defvar dirvish-minibuf-preview--height (- 1 (* max-mini-window-height 1.5))
   "doc")
 
-(defvar danger-minibuf-preview--width nil
+(defvar dirvish-minibuf-preview--width nil
   "doc")
 
-(defvar danger-minibuf-preview-window nil
+(defvar dirvish-minibuf-preview-window nil
   "doc")
 
-(defvar danger-minibuf-preview--category nil
+(defvar dirvish-minibuf-preview--category nil
   "doc")
 
 (defvar selectrum--current-candidate-index)
 
-(defun danger-minibuf-preview-create ()
+(defun dirvish-minibuf-preview-create ()
   "doc"
   (when-let* ((meta (completion-metadata
                      (buffer-substring-no-properties (field-beginning) (point))
                      minibuffer-completion-table
                      minibuffer-completion-predicate))
               (category (completion-metadata-get meta 'category))
-              (show-preview (memq category danger-minibuf-preview-categories)))
-    (setq danger-minibuf-preview--category category)
+              (show-preview (memq category dirvish-minibuf-preview-categories)))
+    (setq dirvish-minibuf-preview--category category)
     (walk-window-tree (lambda (w)
                         (when (window-live-p w)
                           (with-current-buffer (window-buffer w)
                             (let ((ov (make-overlay (point-min) (point-max))))
                               (overlay-put ov 'temp-inactive-ov t)
                               (overlay-put ov 'font-lock-face 'font-lock-doc-face))))))
-    (setq danger-minibuf-preview-window (frame-parameter nil 'danger-preview-window))
-    (unless danger-minibuf-preview-window
-      (let* ((min-w (ceiling (* (frame-width) danger-width-preview)))
-             (min-h (ceiling (* (frame-height) danger-minibuf-preview--height)))
+    (setq dirvish-minibuf-preview-window (frame-parameter nil 'dirvish-preview-window))
+    (unless dirvish-minibuf-preview-window
+      (let* ((min-w (ceiling (* (frame-width) dirvish-width-preview)))
+             (min-h (ceiling (* (frame-height) dirvish-minibuf-preview--height)))
              (b-color (face-attribute 'font-lock-doc-face :foreground))
-             (pos-f (or danger-minibuf-preview-position #'posframe-poshandler-frame-top-center))
+             (pos-f (or dirvish-minibuf-preview-position #'posframe-poshandler-frame-top-center))
              (override `((minibuffer . ,(active-minibuffer-window))))
              (f-props `(:min-width ,min-w :min-height ,min-h :poshandler ,pos-f
                                    :override-parameters ,override
                                    :border-width 5 :border-color ,b-color))
              (frame (apply #'posframe-show "*candidate preview*" f-props)))
-        (setq danger-minibuf-preview-window (frame-root-window frame))
-        (set-window-fringes danger-minibuf-preview-window 30 30 nil t)))
-    (danger-init--buffer)
-    (setq danger-minibuf-preview--width (window-width danger-minibuf-preview-window t))
-    (set-window-dedicated-p danger-minibuf-preview-window nil)))
+        (setq dirvish-minibuf-preview-window (frame-root-window frame))
+        (set-window-fringes dirvish-minibuf-preview-window 30 30 nil t)))
+    (dirvish-init--buffer)
+    (setq dirvish-minibuf-preview--width (window-width dirvish-minibuf-preview-window t))
+    (set-window-dedicated-p dirvish-minibuf-preview-window nil)))
 
-(defun danger-minibuf-preview-teardown ()
+(defun dirvish-minibuf-preview-teardown ()
   "doc"
   (posframe-delete "*candidate preview*")
   (walk-window-tree (lambda (w)
                       (with-current-buffer (window-buffer w)
                         (remove-overlays (point-min) (point-max) 'temp-inactive-ov t))))
-  (setq danger-minibuf-preview--category nil)
-  (setq danger-minibuf-preview--width nil)
-  (mapc 'kill-buffer danger-preview-buffers))
+  (setq dirvish-minibuf-preview--category nil)
+  (setq dirvish-minibuf-preview--width nil)
+  (mapc 'kill-buffer dirvish-preview-buffers))
 
-(defun danger-minibuf--update-advice (fn &rest args)
+(defun dirvish-minibuf--update-advice (fn &rest args)
   "doc"
   (apply fn args)
-  (when-let* ((category danger-minibuf-preview--category)
+  (when-let* ((category dirvish-minibuf-preview--category)
               (cand (cond ((bound-and-true-p vertico-mode)
                            (vertico--candidate))
                           ((bound-and-true-p selectrum-mode)
@@ -115,22 +115,22 @@
         (setq cand (expand-file-name cand (or (cdr-safe (project-current))
                                               (car (minibuffer-history-value)))))
       (setq cand (expand-file-name cand)))
-    (set-frame-parameter nil 'danger-index-path cand)
-    (danger-debounce danger-update--preview danger-preview-delay danger-minibuf-preview-window)))
+    (set-frame-parameter nil 'dirvish-index-path cand)
+    (dirvish-debounce dirvish-update--preview dirvish-preview-delay dirvish-minibuf-preview-window)))
 
 ;;;###autoload
-(define-minor-mode danger-minibuf-preview-mode
-  "Show danger preview when minibuf."
-  :group 'danger :global t
-  (if danger-minibuf-preview-mode
+(define-minor-mode dirvish-minibuf-preview-mode
+  "Show dirvish preview when minibuf."
+  :group 'dirvish :global t
+  (if dirvish-minibuf-preview-mode
       (when window-system
-        (add-hook 'minibuffer-setup-hook #'danger-minibuf-preview-create)
-        (add-hook 'minibuffer-exit-hook #'danger-minibuf-preview-teardown)
-        (advice-add 'vertico--exhibit :around #'danger-minibuf--update-advice)
-        (advice-add 'selectrum--update :around #'danger-minibuf--update-advice))
-    (remove-hook 'minibuffer-setup-hook #'danger-minibuf-preview-create)
-    (remove-hook 'minibuffer-exit-hook #'danger-minibuf-preview-teardown)
-    (advice-remove 'vertico--exhibit #'danger-minibuf--update-advice)
-    (advice-remove 'selectrum--update #'danger-minibuf--update-advice)))
+        (add-hook 'minibuffer-setup-hook #'dirvish-minibuf-preview-create)
+        (add-hook 'minibuffer-exit-hook #'dirvish-minibuf-preview-teardown)
+        (advice-add 'vertico--exhibit :around #'dirvish-minibuf--update-advice)
+        (advice-add 'selectrum--update :around #'dirvish-minibuf--update-advice))
+    (remove-hook 'minibuffer-setup-hook #'dirvish-minibuf-preview-create)
+    (remove-hook 'minibuffer-exit-hook #'dirvish-minibuf-preview-teardown)
+    (advice-remove 'vertico--exhibit #'dirvish-minibuf--update-advice)
+    (advice-remove 'selectrum--update #'dirvish-minibuf--update-advice)))
 
-(provide 'danger-minibuffer)
+(provide 'dirvish-minibuffer)
