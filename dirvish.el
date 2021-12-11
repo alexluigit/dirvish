7c05bfd33e9b29df4ca4ec9a03337bab3364becf
Alex <hellosimon1103@hotmail.com>
2021-12-12 02:13:32 +0800

    Fix docstring
    Former-commit-id: d588e97183c0589626bcf29fc4aa821c4c4fcd22

--- dirvish.el
+++ dirvish.el
@@ -45,7 +45,7 @@
 ;;;; Commands
 
 (defun dirvish-other-buffer ()
-  "Replacement for `mode-line-other-buffer' in dirvish-mode."
+  "Replacement for `mode-line-other-buffer' in `dirvish-mode'."
   (interactive)
   (let ((one-window (frame-parameter nil 'dirvish-one-window)))
     (if one-window
@@ -53,7 +53,7 @@
       (dirvish-find-file (ring-ref dirvish-history-ring 1)))))
 
 (defun dirvish-jump (file)
-  "Replacement for `dired-jump'"
+  "Replacement for `dired-jump' to FILE."
   (interactive (list (read-file-name "Jump to: "))) (dirvish-find-file-dwim file))
 
 (defun dirvish-up-directory ()
@@ -70,18 +70,21 @@
       (dirvish-find-file parent t))))
 
 (defun dirvish-go-top ()
-  "Move to top of file list"
+  "Move to top of file list."
   (interactive)
   (goto-char (point-min)) (dired-next-line 1)
   (dirvish-next-file -1))
 
 (defun dirvish-go-bottom ()
-  "Move to bottom of file list"
+  "Move to bottom of file list."
   (interactive)
   (goto-char (point-max)) (dirvish-next-file 1))
 
 (defun dirvish-next-file (arg)
-  "Move lines in dirvish and initiate updates to preview window."
+  "Move cursor to next line in dirvish and update to preview window.
+
+With optional prefix ARG (\\[universal-argument]), forward ARG
+lines."
   (interactive "^p")
   (dired-next-line arg)
   (cond
@@ -94,11 +97,15 @@
     (dirvish-debounce dirvish-preview-update dirvish-preview-delay)))
 
 (defun dirvish-prev-file (arg)
+  "Same as `dirvish-next-file', only in opposite direction.
+
+With optional prefix ARG (\\[universal-argument]), backward ARG
+lines."
   (interactive "^p")
   (dirvish-next-file (- 0 arg)))
 
 (defun dirvish-show-history (history)
-  "Show history prompt for recent directories"
+  "Prompt for a target directory from HISTORY and goto it."
   (interactive
    (list (completing-read "Select from history: "
                           (cl-remove-duplicates (ring-elements dirvish-history-ring)
@@ -106,7 +113,7 @@
   (when history (dirvish-find-file history)))
 
 (defun dirvish-new-frame (&optional path)
-  "Make a new frame and launch dirvish."
+  "Make a new frame and launch dirvish with optional PATH."
   (interactive (list (read-file-name "Open in new frame: ")))
   (when (with-selected-window (selected-window) (eq major-mode 'dirvish-mode))
     (dirvish-quit))
@@ -115,7 +122,9 @@
     (with-selected-frame frame (dirvish path))))
 
 (defun dirvish-paste (&optional mode)
-  "doc"
+  "Paste marked files/directory to current directory according to MODE.
+
+MODE can be `'copy', `'move', `symlink', or `relalink'."
   (interactive)
   (let* ((regexp (dired-marker-regexp))
          (yanked-files ())
@@ -127,14 +136,18 @@
                               (re-search-forward regexp nil t))
           (setq yanked-files
                 (append yanked-files (dired-map-over-marks (dired-get-filename) nil))))))
-    (unless yanked-files (error "No files marked for paste."))
+    (unless yanked-files (error "No files marked for pasting"))
     (dirvish-internal-paste yanked-files mode)))
 
 (defun dirvish-yank (&optional arg)
+  "Paste marked files/directory to current directory.
+
+With optional prefix ARG, delete source files/directories."
   (interactive "P")
   (if arg (dirvish-paste 'move) (dirvish-paste)))
 
 (defun dirvish-change-level (&optional arg)
+  "Change `dirvish-depth' to ARG."
   (interactive "p")
   (setq dirvish-depth (or arg 1)) (dirvish-refresh t))
 
@@ -178,9 +191,12 @@
       (dirvish-refresh))))
 
 (defun dirvish-init (&optional one-window)
-  "Save previous window config and initialize dirvish."
+  "Save previous window config and initialize dirvish.
+
+If ONE-WINDOW is not-nil, initialize dirvish only in current
+window, not the whole frame."
   (unless (or (posframe-workable-p) one-window)
-    (user-error "dirvish.el: requires GUI."))
+    (user-error "dirvish.el: Requires GUI"))
   (when (eq major-mode 'dirvish-mode) (dirvish-quit))
   (set-frame-parameter nil 'dirvish-one-window one-window)
   (when-let* ((ignore-one-win (not one-window))
@@ -229,7 +245,10 @@
     (setq dirvish-parent-buffers ())))
 
 (defun dirvish-quit (&optional keep-alive)
-  "Revert dirvish settings and disable dirvish."
+  "Revert dirvish settings and disable dirvish.
+
+Delete current frame if it's a dirvish frame unless KEEP-ALIVE
+is not-nil."
   (interactive)
   (dirvish-deinit)
   (when (and (not keep-alive)
@@ -237,8 +256,10 @@
     (delete-frame)))
 
 (defun dirvish-refresh (&optional rebuild filter no-revert)
-  "Reset dirvish. With optional prefix ARG (\\[universal-argument])
-also rebuild dirvish layout."
+  "Reset dirvish.
+If REBUILD is not-nil, rebuild dirvish layout;
+If FILTER is not-nil, update dirvish file filter;
+Unless NO-REVERT is not-nil, revert current buffer."
   (interactive "P")
   (when rebuild
     (dirvish-parent-build)
@@ -255,8 +276,8 @@ also rebuild dirvish layout."
   "Find file in dirvish buffer.
 
 FILE can be a file or a directory, if nil then infer entry from
-current `buffer-file-name'. If IGNORE-HIST is non-nil, do not
-update `dirvish-history-ring'."
+current `(variable) buffer-file-name'.  If IGNORE-HIST is
+non-nil, do not update `dirvish-history-ring'."
   (interactive)
   (let ((entry (or file (dired-get-filename nil t)))
         (bname (buffer-file-name (current-buffer)))
@@ -279,7 +300,7 @@ update `dirvish-history-ring'."
 
 ;;;###autoload
 (defun dirvish-find-file-dwim (&rest args)
-  "Call `dirvish-find-file' or `dired-find-file'."
+  "Apply `dirvish-find-file' or `dired-find-file' with ARGS."
   (if (derived-mode-p 'dirvish-mode)
       (apply 'dirvish-find-file args)
     (apply 'find-alternate-file args)))
@@ -294,7 +315,9 @@ update `dirvish-history-ring'."
 
 ;;;###autoload
 (defun dirvish (&optional path one-window)
-  "Launch dired in dirvish-mode."
+  "Open dirvish in PATH, optionally in single-window if ONE-WINDOW is not-nil.
+
+PATH defaults to `(variable)buffer-file-name'."
   (interactive)
   (let* ((file (or path buffer-file-name))
          (dir (if file (expand-file-name (file-name-directory file))
