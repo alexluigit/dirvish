d9ff6d8cbf205710bb053d23ae396abfb118d847
Alex <hellosimon1103@hotmail.com>
2021-12-27 14:51:21 +0800

    minibuffer-preview: remove posframe dependency

--- dirvish-minibuffer-preview.el
+++ dirvish-minibuffer-preview.el
@@ -15,23 +15,12 @@
 (require 'dirvish)
 (require 'find-func)
 
-(defcustom dirvish-minibuf-preview-position
-  (lambda (info)
-    (cons (/ (- (plist-get info :parent-frame-width)
-                (plist-get info :posframe-width)) 2)
-          (or (* (frame-parameter nil 'internal-border-width) 2) 60)))
-  "A function determines position of dirvish minibuffer preview window.
-
-Used as `:poshandler' for `posframe-show'."
-  :group 'dirvish :type 'function)
-
 (defvar dirvish-minibuf-preview-categories '(file project-file library))
-(defvar dirvish-minibuf-preview--height (- 1 (* max-mini-window-height 1.5)))
-(defvar dirvish-minibuf-preview--width nil)
 (defvar dirvish-minibuf-preview-window nil)
 (defvar dirvish-minibuf-preview--category nil)
 (defvar selectrum--current-candidate-index)
 (defvar dirvish-preview-update-timer)
+(defvar dirvish-minibuf-use-exist-window nil)
 
 (defun dirvish-minibuf-preview-create ()
   "Create dirvish minibuffer preview window using `posframe'."
@@ -42,39 +31,22 @@ Used as `:poshandler' for `posframe-show'."
               (category (completion-metadata-get meta 'category))
               (show-preview (memq category dirvish-minibuf-preview-categories)))
     (setq dirvish-minibuf-preview--category category)
-    (walk-window-tree (lambda (w)
-                        (when (window-live-p w)
-                          (with-current-buffer (window-buffer w)
-                            (let ((ov (make-overlay (point-min) (point-max))))
-                              (overlay-put ov 'temp-inactive-ov t)
-                              (overlay-put ov 'font-lock-face 'font-lock-doc-face))))))
-    (or (frame-parameter nil 'dirvish-meta)
-        (set-frame-parameter nil 'dirvish-meta (make--dirvish)))
-    (setq dirvish-minibuf-preview-window (dirvish-preview-window (dirvish-meta)))
-    (unless (window-live-p dirvish-minibuf-preview-window)
-      (let* ((min-w (ceiling (* (frame-width) dirvish-preview-width)))
-             (min-h (ceiling (* (frame-height) dirvish-minibuf-preview--height)))
-             (b-color (face-attribute 'font-lock-doc-face :foreground))
-             (pos-f (or dirvish-minibuf-preview-position #'posframe-poshandler-frame-top-center))
-             (override `((minibuffer . ,(active-minibuffer-window))))
-             (f-props `(:min-width ,min-w :min-height ,min-h :poshandler ,pos-f
-                                   :override-parameters ,override
-                                   :border-width 5 :border-color ,b-color))
-             (frame (apply #'posframe-show "*candidate preview*" f-props)))
-        (setq dirvish-minibuf-preview-window (frame-root-window frame))
-        (set-window-fringes dirvish-minibuf-preview-window 30 30 nil t)))
-    (setq dirvish-minibuf-preview--width (window-width dirvish-minibuf-preview-window t))
-    (set-window-dedicated-p dirvish-minibuf-preview-window nil)))
+    (if-let ((preview-win (and (dirvish-curr) (dv-preview-window (dirvish-curr)))))
+        (progn
+          (setq dirvish-minibuf-use-exist-window t)
+          (setq dirvish-minibuf-preview-window preview-win))
+      (dirvish-activate t)
+      (let ((next-win (next-window)))
+        (setq dirvish-minibuf-preview-window next-win)
+        (setf (dv-preview-pixel-width (dirvish-curr)) (window-width next-win t))))))
 
 (defun dirvish-minibuf-preview-teardown ()
   "Teardown dirvish minibuffer preview window."
-  (posframe-delete "*candidate preview*")
-  (walk-window-tree (lambda (w)
-                      (with-current-buffer (window-buffer w)
-                        (remove-overlays (point-min) (point-max) 'temp-inactive-ov t))))
+  (when (and dirvish-minibuf-preview--category
+             (not dirvish-minibuf-use-exist-window))
+    (dirvish-deactivate))
   (setq dirvish-minibuf-preview--category nil)
-  (setq dirvish-minibuf-preview--width nil)
-  (mapc #'kill-buffer dirvish-preview-buffers))
+  (setq dirvish-minibuf-use-exist-window nil))
 
 (defun dirvish--minibuf-update-advice (fn &rest args)
   "Apply FN with ARGS, then update dirvish minibuffer preview window.
@@ -96,8 +68,8 @@ invoked when file name under cursor in minibuffer changed."
        (setq cand (expand-file-name cand (or (cdr-safe (project-current))
                                              (car (minibuffer-history-value))))))
       ('library
-       (setq cand (ignore-errors (find-library-name cand)))))
-    (setf (dirvish-index-path (dirvish-meta)) cand)
+       (setq cand (file-truename (or (ignore-errors (find-library-name cand)) "")))))
+    (setf (dv-index-path (dirvish-curr)) cand)
     (dirvish-debounce dirvish-preview-update
                       dirvish-preview-delay
                       dirvish-minibuf-preview-window)))
