5fbdd7fd9aed7c15e4dabf4b05d95ad1043a92c4
Alex <hellosimon1103@hotmail.com>
2022-01-05 03:29:32 +0800

    chore: bump version to 0.9.6

--- dirvish.el
+++ dirvish.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2021-2022 Alex Lu
 
 ;; Author : Alex Lu <https://github.com/alexluigit>
-;; Version: 0.9.5
+;; Version: 0.9.6
 ;; Keywords: files, convenience
 ;; Homepage: https://github.com/alexluigit/dirvish
 ;; SPDX-License-Identifier: GPL-3.0-or-later
