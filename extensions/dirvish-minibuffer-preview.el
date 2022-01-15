2ac118a2c488cfa285925b9a7cf954c7cd94c146
Alex <hellosimon1103@hotmail.com>
2022-01-15 17:27:07 +0800

    refactor(peek-mode): remove runtime deps check

--- extensions/dirvish-minibuffer-preview.el
+++ extensions/dirvish-minibuffer-preview.el
@@ -16,50 +16,57 @@
 
 (declare-function selectrum--get-candidate "selectrum")
 (declare-function selectrum--get-full "selectrum")
+(declare-function selectrum--update "selectrum")
 (declare-function vertico--candidate "vertico")
+(declare-function vertico--exhibit "vertico")
+
+(defvar dirvish--minibuf-ad-sym 'vertico--exhibit)
+(defvar dirvish--minibuf-cand-fn #'vertico--candidate)
+(defvar selectrum--current-candidate-index)
 
 (require 'dirvish)
 (require 'find-func)
 
-(defvar dirvish-minibuf-preview-categories '(file project-file library))
-(defvar dirvish-minibuf-preview--category nil)
-(defvar selectrum--current-candidate-index)
+(defcustom dirvish-minibuf-preview-categories '(file project-file library)
+  "Minibuffer metadata categories to show file preview."
+  :group 'dirvish :type 'list)
 
 (defun dirvish-minibuf-preview-create ()
   "Create dirvish minibuffer preview window.
 The window is created only when metadata in current minibuffer is
 one of categories in `dirvish-minibuf-preview-categories'."
-  (when-let* ((meta (completion-metadata
-                     (buffer-substring-no-properties (field-beginning) (point))
-                     minibuffer-completion-table
-                     minibuffer-completion-predicate))
-              (category (completion-metadata-get meta 'category))
-              (show-preview (memq category dirvish-minibuf-preview-categories)))
-    (setq dirvish-minibuf-preview--category category)
-    (unless (and (dirvish-curr) (dv-preview-window (dirvish-curr)))
-      (set-frame-parameter nil 'dirvish--minibuf (dirvish-activate 0))
+  (let* ((old-dv (dirvish-curr))
+         (meta (completion-metadata
+                (buffer-substring-no-properties (field-beginning) (point))
+                minibuffer-completion-table
+                minibuffer-completion-predicate))
+         (category (completion-metadata-get meta 'category))
+         (preview-category (and (memq category dirvish-minibuf-preview-categories) category))
+         new-dv)
+    (when (and preview-category
+               (not (and old-dv (dv-preview-window old-dv))))
+      (setq new-dv (dirvish-activate 0))
       (let ((next-win (next-window)))
-        (setf (dv-preview-window (dirvish-curr)) next-win)))))
+        (setf (dv-preview-window new-dv) next-win)))
+    (set-frame-parameter nil 'dirvish--minibuf
+                         `(:category ,preview-category :old ,old-dv :new ,new-dv))))
 
 (defun dirvish-minibuf-preview-teardown ()
   "Teardown dirvish minibuffer preview window."
-  (when-let (dv (frame-parameter nil 'dirvish--minibuf))
-    (dirvish-deactivate dv))
-  (setq dirvish-minibuf-preview--category nil))
+  (let* ((dv-mini (frame-parameter nil 'dirvish--minibuf))
+         (old-dv (plist-get dv-mini :old))
+         (new-dv (plist-get dv-mini :new)))
+    (when new-dv (dirvish-deactivate new-dv))
+    (set-frame-parameter nil 'dirvish--curr old-dv)))
 
-(defun dirvish--minibuf-update-advice (fn &rest args)
+(defun dirvish--minibuf-update-advice (&rest _)
   "Apply FN with ARGS, then update dirvish minibuffer preview window.
 
 Used as an advice for `vertico--exhibit' or `selectrum--update',
 invoked when file name under cursor in minibuffer changed."
-  (apply fn args)
-  (when-let* ((category dirvish-minibuf-preview--category)
-              (cand (cond ((bound-and-true-p vertico-mode)
-                           (vertico--candidate))
-                          ((bound-and-true-p selectrum-mode)
-                           (selectrum--get-full
-                            (selectrum--get-candidate
-                             selectrum--current-candidate-index))))))
+  (when-let* ((category (plist-get
+                         (frame-parameter nil 'dirvish--minibuf) :category))
+              (cand (funcall dirvish--minibuf-cand-fn)))
     (pcase category
       ('file
        (setq cand (expand-file-name cand)))
@@ -79,12 +86,19 @@ invoked when file name under cursor in minibuffer changed."
       (progn
         (add-hook 'minibuffer-setup-hook #'dirvish-minibuf-preview-create)
         (add-hook 'minibuffer-exit-hook #'dirvish-minibuf-preview-teardown)
-        (advice-add 'vertico--exhibit :around #'dirvish--minibuf-update-advice)
-        (advice-add 'selectrum--update :around #'dirvish--minibuf-update-advice))
+        (advice-add dirvish--minibuf-ad-sym :after #'dirvish--minibuf-update-advice))
     (remove-hook 'minibuffer-setup-hook #'dirvish-minibuf-preview-create)
     (remove-hook 'minibuffer-exit-hook #'dirvish-minibuf-preview-teardown)
-    (advice-remove 'vertico--exhibit #'dirvish--minibuf-update-advice)
-    (advice-remove 'selectrum--update #'dirvish--minibuf-update-advice)))
+    (advice-remove dirvish--minibuf-ad-sym #'dirvish--minibuf-update-advice)))
+
+(with-eval-after-load 'selectrum
+  (let ((enabled dirvish-minibuf-preview-mode))
+    (and enabled (dirvish-minibuf-preview-mode -1))
+    (setq dirvish--minibuf-ad-sym 'selectrum--update)
+    (setq dirvish--minibuf-cand-fn
+          (lambda () (selectrum--get-full (selectrum--get-candidate
+                                      selectrum--current-candidate-index))))
+    (and enabled (dirvish-minibuf-preview-mode +1))))
 
 (provide 'dirvish-minibuffer-preview)
 ;;; dirvish-minibuffer-preview.el ends here
