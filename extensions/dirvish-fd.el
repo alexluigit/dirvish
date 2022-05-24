75e214cb4084948c6aae172fe9ea0b50b99ba0c2
Alex <hellosimon1103@hotmail.com>
2022-05-24 20:05:40 +0800

    feat(fd): add dirvish-fd.el, drop `fd-dired.el` support

---
+++ extensions/dirvish-fd.el
@@ -0,0 +1,108 @@
+;;; dirvish-fd.el --- find-dired alternative using fd  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021-2022 Alex Lu
+;; Author : Alex Lu <https://github.com/alexluigit>
+;; Version: 1.3.21
+;; Keywords: files, convenience
+;; Homepage: https://github.com/alexluigit/dirvish
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;;; Commentary:
+
+;; `fd' integration for Dirvish.
+
+;;; Code:
+
+(require 'dirvish)
+
+(defcustom dirvish-fd-switches ""
+  "Fd arguments inserted before user input."
+  :type 'string :group 'dirvish)
+
+(defcustom dirvish-fd-ls-program
+  (if-let ((gls (executable-find "gls"))) gls insert-directory-program)
+  "Listing program for `fd'."
+  :type '(string :tag "Listing program, such as `ls'") :group 'dirvish
+  :set (lambda (k v)
+         (set k v)
+         (and (eq system-type 'darwin) (not (executable-find "gls"))
+              (warn "Please install `ls' from coreutils with 'brew install coreutils'"))))
+
+(defvar dirvish-fd-program "fd" "The default fd program.")
+(defvar dirvish-fd-args-history nil "History list of fd arguments entered in the minibuffer.")
+(defvar dirvish-fd--buf-name-fmt "*Dirvish-FD@@%s@@%s@@%s*")
+(defvar dirvish-fd-last-input "" "Last used fd arguments.")
+(defvar-local dirvish-fd-actual-switches nil)
+(put 'dirvish-fd-actual-switches 'permanent-local t)
+
+(defun dirvish-fd-proc-filter (proc string)
+  "Filter for `dirvish-fd' processes PROC and output STRING."
+  (let ((buf (process-buffer proc))
+	      (inhibit-read-only t))
+    (if (buffer-name buf)
+	      (with-current-buffer buf
+	        (save-excursion
+	          (save-restriction
+	            (widen)
+	            (let ((buffer-read-only nil)
+		                (beg (point-max)))
+		            (goto-char beg)
+		            (insert string)
+		            (goto-char beg)
+		            (or (looking-at "^") (forward-line 1))
+		            (while (looking-at "^") (insert "  ") (forward-line 1))
+		            (goto-char (- beg 3))	; no error if < 0
+		            (while (search-forward " ./" nil t)
+		              (delete-region (point) (- (point) 2)))))))
+      (delete-process proc))))
+
+;;;###autoload
+(defun dirvish-fd (dir pattern)
+  "Run `fd' on DIR and go into Dired mode on a buffer of the output.
+The command run is essentially:
+
+  fd --color=never -0 `dirvish-fd-switches' PATTERN
+     --exec-batch `dirvish-fd-ls-program' `dired-listing-switches' --directory."
+  ;; In case users issue a new `dirvish-fd' when already in a result buffer
+  (interactive (list (and current-prefix-arg
+                          (read-directory-name "Fd target directory: " nil "" t))
+                     (read-string "Fd search pattern: " dirvish-fd-last-input
+                                  '(dirvish-fd-args-history . 1))))
+  (when (and (or (not dir) current-prefix-arg)
+             dirvish-fd-actual-switches)
+    (setq dirvish-fd-actual-switches nil))
+  (setq dir (file-name-as-directory (expand-file-name (or dir default-directory))))
+  (or (file-directory-p dir) (user-error "'fd' command requires a directory: %s" dir))
+  (let* ((reuse (when (dirvish-prop :fd-dir) (current-buffer)))
+         (buf-name (format dirvish-fd--buf-name-fmt dir pattern (make-temp-name "")))
+         (buffer (or reuse (get-buffer-create buf-name))))
+    (pop-to-buffer-same-window buffer)
+    (with-current-buffer buffer
+      (widen)
+      (let ((ls-switches (or dired-actual-switches
+                             (and reuse (dv-ls-switches (dirvish-curr)))
+                             dired-listing-switches))
+            (fmt "%s -0 --color=never %s --exec-batch %s %s --quoting-style=literal --directory &")
+            buffer-read-only)
+        (erase-buffer)
+        (setq default-directory dir)
+        (setq dirvish-fd-last-input pattern) ; save for next interactive call
+        (setq dirvish-fd-actual-switches
+              (or dirvish-fd-actual-switches (format "%s %s" dirvish-fd-switches pattern)))
+        (shell-command (format fmt dirvish-fd-program dirvish-fd-actual-switches
+                               dirvish-fd-ls-program ls-switches)
+                       buffer)
+        (dired-mode dir ls-switches)
+        (setq-local revert-buffer-function
+                    (lambda (_ignore-auto _noconfirm)
+                      (dirvish-fd default-directory dirvish-fd-last-input)))
+        (setq-local dired-subdir-alist (list (cons default-directory (point-min-marker))))
+        (let (buffer-read-only) (insert "  " dir ":\n")))
+      (let ((proc (get-buffer-process buffer)))
+        (set-process-filter proc #'dirvish-fd-proc-filter)
+        (set-process-sentinel proc #'dirvish-find-dired-sentinel-ad)
+        ;; Initialize the process marker; it is used by the filter.
+        (move-marker (process-mark proc) (point) buffer)))))
+
+(provide 'dirvish-fd)
+;;; dirvish-fd.el ends here
