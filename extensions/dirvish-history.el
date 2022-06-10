fd0490e3c2d30a7cc9b97418096f1bdcd9fa7835
Alex <hellosimon1103@hotmail.com>
2022-06-10 14:47:00 +0800

    refactor: use `dirvish-find-entry-hook` for history insertion

--- extensions/dirvish-history.el
+++ extensions/dirvish-history.el
@@ -20,13 +20,33 @@
 (define-obsolete-function-alias 'dirvish-go-forward-history #'dirvish-history-go-forward "Jun 08, 2022")
 (define-obsolete-function-alias 'dirvish-go-backward-history #'dirvish-history-go-backward "Jun 08, 2022")
 
+(defvar dirvish-history--ring nil)
+(defun dirvish-history-insert-entry-h (entry buffer)
+  "Add ENTRY name for BUFFER to `dirvish-history--ring'."
+  (let ((entry (if (string-prefix-p "FD####" entry)
+                   (buffer-name buffer) entry)))
+    (ring-insert dirvish-history--ring entry)))
+(defcustom dirvish-history-length 50
+  "Length of directory visiting history Dirvish will track."
+  :group 'dirvish :type 'integer
+  :set (lambda (k v)
+         (set k v)
+         (if (> dirvish-history-length 0)
+             (progn
+               (setq dirvish-history--ring (make-ring v))
+               (add-hook 'dirvish-find-entry-hook #'dirvish-history-insert-entry-h))
+           (setq dirvish-history--ring nil)
+           (remove-hook 'dirvish-find-entry-hook #'dirvish-history-insert-entry-h))))
+
 ;;;###autoload
 (defun dirvish-history-jump ()
-  "Open a target directory from `dirvish--history-ring'."
+  "Open a target directory from `dirvish-history--ring'."
   (interactive)
+  (unless (ring-p dirvish-history--ring)
+    (user-error "Dirvish[error]: global history tracking has been disabled"))
   (let* ((history-w/metadata
           (dirvish--append-metadata
-           'file (ring-elements dirvish--history-ring)))
+           'file (ring-elements dirvish-history--ring)))
          (result (completing-read "Recently visited: " history-w/metadata)))
       (when result (dirvish-find-file result))))
 
@@ -34,10 +54,12 @@
 (defun dirvish-history-last ()
   "Switch to the most recently visited dirvish buffer."
   (interactive)
+  (unless (ring-p dirvish-history--ring)
+    (user-error "Dirvish[error]: global history tracking has been disabled"))
   (let ((match
          (cl-loop
           with local-entries = (mapcar #'car (dv-roots (dirvish-curr)))
-          with entries = (ring-elements dirvish--history-ring)
+          with entries = (ring-elements dirvish-history--ring)
           for entry in entries
           thereis (and (member entry local-entries)
                        (not (equal entry (dired-current-directory))) entry))))
