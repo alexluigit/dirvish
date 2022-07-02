5adf9fcb1236a320fdfb78cd64e4bfb252254c42
Alex <hellosimon1103@hotmail.com>
2022-07-02 15:03:38 +0800

    fix(narrow): drop the overlay approach, accommodate subtree (#67)
    fix(narrow): remove subtrees before narrowing (#67)
    
    
    Former-commit-id: 09ffe10b3d4aa77f66bba36e92261a3d8c6f947c

--- extensions/dirvish-narrow.el
+++ extensions/dirvish-narrow.el
@@ -15,48 +15,72 @@
 ;;; Code:
 
 (require 'dirvish)
+(declare-function dirvish-subtree--revert "dirvish-subtree")
+
+(defvar-local dirvish-narrow--subdir-alist '())
+
+(defun dirvish-narrow--build-indices ()
+  "Update the Dirvish buffer based on the input of the minibuffer."
+  (setq dirvish-narrow--subdir-alist '())
+  (when (bound-and-true-p dirvish-subtree--overlays)
+    (dirvish-subtree--revert t))
+  (with-current-buffer (window-buffer (minibuffer-selected-window))
+      (cl-loop for (dir . beg) in dired-subdir-alist do
+               (progn (goto-char beg)
+                      (forward-line (if dirvish--dired-free-space 2 1))
+                      (dirvish-narrow--index-subdir dir (dired-subdir-max))))))
 
 (defun dirvish-narrow-dirvish-update-h ()
   "Update the Dirvish buffer based on the input of the minibuffer."
   (dirvish-debounce 'narrow
     (let ((input (minibuffer-contents-no-properties)))
       (with-current-buffer (window-buffer (minibuffer-selected-window))
-        (cl-loop for (_dir . pos) in dired-subdir-alist do
-                 (progn (goto-char pos)
-                        (forward-line (if dirvish--dired-free-space 2 1))
-                        (dirvish-narrow-subdir input pos (dired-subdir-max))))
+        (cl-loop for idx from 0
+                 for (dir . pos) in dired-subdir-alist
+                 do (progn (goto-char pos)
+                           (forward-line (if dirvish--dired-free-space 2 1))
+                           (dirvish-narrow--filter-subdir dir input idx)))
+        (goto-char (window-start))
+        (dired-goto-file (dirvish-prop :child))
         (dirvish-update-body-h)))))
 
-(cl-defun dirvish-narrow-subdir (regex beg end)
-  "Filter the subdir from BEG to END with REGEX."
-  (remove-overlays beg end 'dirvish-narrow-ov t)
-  (when (equal regex "") (cl-return-from dirvish-narrow-subdir))
-  (while (< (point) end)
-    (when-let ((f-beg (dired-move-to-filename))
-               (f-end (dired-move-to-end-of-filename)))
-      (let* ((f-name (buffer-substring-no-properties f-beg f-end))
-             (match (string-match regex f-name)))
-        (unless match
-          (let* ((l-beg (line-beginning-position))
-                 (l-end (line-end-position))
-                 (ov (make-overlay l-beg (1+ l-end))))
-            (overlay-put ov 'dirvish-narrow-ov t)
-            (overlay-put ov 'invisible t)
-            (overlay-put ov 'evaporate t)))))
-    (forward-line 1)))
+(cl-defun dirvish-narrow--index-subdir (subdir end)
+  "Filter the SUBDIR from BEG to END."
+  (let (files)
+    (while (< (point) end)
+      (when-let* ((f-beg (dired-move-to-filename))
+                  (f-end (dired-move-to-end-of-filename))
+                  (f-name (buffer-substring-no-properties f-beg f-end))
+                  (l-beg (line-beginning-position))
+                  (l-end (1+ (line-end-position)))
+                  (l-str (buffer-substring l-beg l-end)))
+        (push (cons f-name l-str) files))
+      (forward-line 1))
+    (push (cons subdir (reverse files)) dirvish-narrow--subdir-alist)))
+
+(defun dirvish-narrow--filter-subdir (dir regex idx)
+  "Filter the subdir DIR with REGEX.
+IDX the index of DIR in `dired-subdir-alist'."
+  (let ((files (alist-get dir dirvish-narrow--subdir-alist nil nil #'equal))
+        (p-max (- (dired-subdir-max) (if (eq idx 0) 0 1)))
+        buffer-read-only)
+    (delete-region (point) p-max)
+    (cl-loop for (file . line) in files do
+             (when (string-match regex file)
+               (insert line)))))
 
 ;;;###autoload
-(defun dirvish-narrow (&optional show)
-  "Narrow a Dirvish buffer to the files matching a regex.
-With optional prefix argument SHOW reveal the hidden lines."
-  (interactive "P")
-  (if show
-      (progn (remove-overlays (point-min) (point-max) 'dirvish-narrow-ov t)
-             (message "All files revealed"))
-    (when (minibufferp) (user-error "`%s' called inside the minibuffer" this-command))
+(defun dirvish-narrow ()
+  "Narrow a Dirvish buffer to the files matching a regex."
+  (interactive)
+  (dirvish-narrow--build-indices)
+  (when (minibufferp) (user-error "`%s' called inside the minibuffer" this-command))
+  (let ((old-f (dirvish-prop :child)))
     (minibuffer-with-setup-hook
         (lambda () (add-hook 'post-command-hook #'dirvish-narrow-dirvish-update-h nil t))
-      (read-from-minibuffer "Focus on files: "))))
+      (unwind-protect
+          (read-from-minibuffer "Focus on files: ")
+        (dired-goto-file old-f)))))
 
 (provide 'dirvish-narrow)
 ;;; dirvish-narrow.el ends here
