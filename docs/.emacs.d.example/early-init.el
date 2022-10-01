252764fe5f974d32835d20dedf08265df05ad8b2
Alex <hellosimon1103@hotmail.com>
2022-10-01 18:01:23 +0800

    docs: add instructions for Emacs newcomers

---
+++ docs/.emacs.d.example/early-init.el
@@ -0,0 +1,30 @@
+(setq
+ gc-cons-threshold 134217728
+ inhibit-startup-message t ; Reduce noise at startup
+ inhibit-startup-echo-area-message user-login-name
+ inhibit-startup-screen t
+ inhibit-default-init t
+ initial-scratch-message nil
+ auto-mode-case-fold nil ; Use case-sensitive `auto-mode-alist' for performance
+ fast-but-imprecise-scrolling t ; More performant rapid scrolling over unfontified regions
+ ffap-machine-p-known 'reject ; Don't ping things that look like domain names
+ frame-inhibit-implied-resize t ; Inhibit frame resizing for performance
+ idle-update-delay 1.0  ; slow down UI updates down
+ inhibit-compacting-font-caches t ; Inhibit frame resizing for performance
+ read-process-output-max (* 1024 1024) ; Increase how much is read from processes in a single chunk.
+ redisplay-skip-fontification-on-input t ; Inhibits it for better scrolling performance.
+ command-line-x-option-alist nil ; Remove irreleant command line options for faster startup
+ select-active-regions 'only ; Emacs hangs when large selections contain mixed line endings.
+ auto-save-list-file-prefix nil ; Disable auto-save
+ create-lockfiles nil ; Disable lockfiles
+ make-backup-files nil ; Disable backup files
+ vc-follow-symlinks t ; Do not ask about symlink following
+ custom-file (concat user-emacs-directory "custom.el") ; Place all "custom" code in a temporary file
+ use-short-answers t ; y/n for yes/no
+ package-quickstart nil ; Prevent package.el loading packages prior to their init-file
+ package-enable-at-startup nil)
+
+(tool-bar-mode -1)                    ; Disable toolbar
+(tooltip-mode -1)                     ; Disable tooltips
+(menu-bar-mode -1)                    ; Disable menu bar
+(scroll-bar-mode -1)                  ; Disable scroll bar
---
+++ docs/.emacs.d.example/init.el
@@ -0,0 +1,89 @@
+;;; init.el --- Emacs init file -*- lexical-binding: t -*-
+
+;;;; setup package manager
+(setq
+ straight-vc-git-default-clone-depth 1
+ straight-check-for-modifications '(check-on-save find-when-checking)
+ straight-repository-branch "develop")
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;;; install packages
+(straight-use-package 'bind-key)
+(straight-use-package 'dirvish)
+(straight-use-package 'all-the-icons)
+(straight-use-package 'diredfl)
+(straight-use-package 'vertico)
+(straight-use-package 'orderless)
+(straight-use-package 'consult)
+(straight-use-package 'ef-themes)
+(let ((cache (expand-file-name
+              "all-the-icons-font-installed" user-emacs-directory)))
+  (unless (file-exists-p cache)
+    (all-the-icons-install-fonts t)
+    (with-temp-buffer (write-file cache))))
+
+;;;; setup packages
+
+;;;;; appearance
+(require 'ef-themes)
+(load-theme 'ef-night t)
+(add-hook 'dired-mode-hook 'diredfl-mode)
+(add-hook 'dirvish-directory-view-mode-hook 'diredfl-mode)
+(with-eval-after-load 'diredfl
+  (set-face-attribute 'diredfl-dir-name nil :bold t))
+
+;;;;; minibuffer
+(vertico-mode 1)
+(setq completion-styles '(orderless))
+(setq orderless-component-separator #'orderless-escapable-split-on-space)
+(setq orderless-matching-styles
+      '(orderless-initialism orderless-prefixes orderless-regexp))
+
+;;;;; dirvish
+(dirvish-override-dired-mode)
+(dirvish-peek-mode)
+(setq dirvish-attributes
+      '(vc-state file-size git-msg subtree-state all-the-icons collapse file-time))
+(setq dirvish-mode-line-format '(:left (sort symlink) :right (vc-info yank index)))
+(setq dirvish-header-line-height '(25 . 35))
+(setq dirvish-side-width 38)
+(setq dirvish-header-line-format '(:left (path) :right (free-space)))
+(setq dired-listing-switches
+      "-g --almost-all --human-readable --time-style=long-iso --group-directories-first --no-group")
+(bind-keys ("C-c f" . dirvish-fd)
+           :map 'dirvish-mode-map
+           ;; left click for expand/collapse dir or open file
+           ("<mouse-1>" . dirvish-subtree-toggle-or-open)
+           ;; middle click for opening file / entering dir in other window
+           ("<mouse-2>" . dired-mouse-find-file-other-window)
+           ;; right click for opening file / entering dir
+           ("<mouse-3>" . dired-mouse-find-file)
+           ([remap dired-sort-toggle-or-edit] . dirvish-quicksort)
+           ([remap dired-do-redisplay] . dirvish-ls-switches-menu)
+           ([remap dired-do-copy] . dirvish-yank-menu)
+           ("?"   . dirvish-dispatch)
+           ("q"   . dirvish-quit)
+           ("a"   . dirvish-quick-access)
+           ("f"   . dirvish-file-info-menu)
+           ("x"   . dired-do-delete)
+           ("X"   . dired-do-flagged-delete)
+           ("y"   . dirvish-yank-menu)
+           ("s"   . dirvish-quicksort)
+           ("TAB" . dirvish-subtree-toggle)
+           ("M-t" . dirvish-layout-toggle)
+           ("M-b" . dirvish-history-go-backward)
+           ("M-f" . dirvish-history-go-forward)
+           ("M-n" . dirvish-narrow)
+           ("M-m" . dirvish-mark-menu)
+           ("M-s" . dirvish-setup-menu)
+           ("M-e" . dirvish-emerge-menu))
